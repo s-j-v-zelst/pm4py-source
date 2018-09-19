71c29348c7ee59ee5bf062b5f505c6abae052a73
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-09-19 16:51:24 +0200

    intermediate cointermediary commit (for remote merge)

--- pm4py/algo/prediction/next_activity/transition_system_based/predictor.py
+++ pm4py/algo/prediction/next_activity/transition_system_based/predictor.py
@@ -9,8 +9,9 @@ class TransitionSystemBasedNextActivityPredictor:
         if pm4py.util.constants.PARAMETER_CONSTANT_ACTIVITY_KEY not in parameters:
             parameters[pm4py.util.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = pm4py.log.util.xes.DEFAULT_NAME_KEY
         self.transition_system = pm4py.algo.transition_system.factory.apply(trace_log, parameters=parameters)
-        self.activity_distribution = pm4py.log.util.trace_log.get_event_labels_counted(trace_log, pm4py.log.util.xes.DEFAULT_NAME_KEY)
-
+        activity_distribution = pm4py.log.util.trace_log.get_event_labels_counted(trace_log, pm4py.log.util.xes.DEFAULT_NAME_KEY)
+        summ = sum(activity_distribution.values())
+        self.activity_distribution = {k: activity_distribution[k] / summ for k in activity_distribution.keys()}
 
 
 
@@ -19,4 +20,5 @@ class TransitionSystemBasedNextActivityPredictor:
 if __name__ == '__main__':
     log = pm4py.log.importer.xes.factory.apply('C:/Users/bas/Documents/tue/svn/private/logs/a32_logs/a32f0n05.xes')
     pred = TransitionSystemBasedNextActivityPredictor(log)
+    viz = pm4py.models.transition_system.
     print(pred.activity_distribution)
